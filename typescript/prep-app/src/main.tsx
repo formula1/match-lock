b4cf304623153e9f85bba494151a0d5cccf34c9c
formula1 <samtobia@gmail.com>
2025-11-07 14:18:51 -0800

    PrepApp Style: add a global css

--- typescript/prep-app/src/main.tsx
+++ typescript/prep-app/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './tauri' // Initialize Tauri API compatibility layer
+import './styles/global.css' // Global styles
 
 console.log("Hello World!")
 
