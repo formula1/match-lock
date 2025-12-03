13a468eefba7147a988d46c5ab2f7f0b8736ff1a
formula1 <samtobia@gmail.com>
2025-12-02 21:57:28 -0800

    Code Editor Refactor: PathVariables is now a folder so we can include a tooltip explaining it's purpose

--- core/config-editor/src/pages/engine-config/Form/PieceDefinitions/PathVariablesInput/index.tsx
+++ core/config-editor/src/pages/engine-config/Form/PieceDefinitions/PathVariablesInput/index.tsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
-import type { InputProps } from "../../../../utils/react/input";
+import type { InputProps } from "../../../../../utils/react/input";
+import { validatePathVariable } from "@match-lock/shared";
+
+import PathVariablesTT from "./explain.md";
 
 export function PathVariablesInput({ value, onChange }: InputProps<Array<string>>){
-  return <div>
-    <h3>Path Variables</h3>
+  return <>
+    <h3><ToolTipSpan tip={PathVariablesTT}>Path Variables</ToolTipSpan></h3>
     <PathVariableCreator value={value} onChange={onChange} />
     <div className="section">
       {value.map((variable, i) => (
         <PathVariableItem key={`${variable}-${i}`} index={i} value={value} onChange={onChange} />
       ))}
     </div>
-  </div>
+  </>
 }
 
 function PathVariableCreator({ value, onChange }: InputProps<Array<string>>){
@@ -21,7 +24,6 @@ function PathVariableCreator({ value, onChange }: InputProps<Array<string>>){
         try {
           e.preventDefault();
           if(error !== null) return;
-          if(newVariable === "") throw new Error("Variable name cannot be empty");
           onChange([...value, newVariable]);
           setNewVariable("");
         }catch(error){
@@ -32,9 +34,14 @@ function PathVariableCreator({ value, onChange }: InputProps<Array<string>>){
         type="text"
         value={newVariable}
         onChange={(e)=>{
-          setNewVariable(e.target.value)
-          setError(null);
-          if(value.includes(e.target.value)) return setError("Variable already exists");
+          try {
+            setNewVariable(e.target.value)
+            setError(null);
+            if(value.includes(e.target.value)) throw new Error("Variable already exists");
+            validatePathVariable(e.target.value);
+          }catch(error){
+            setError((error as Error).message);
+          }
         }}
       />
       <button type="submit">Add</button>
@@ -43,7 +50,7 @@ function PathVariableCreator({ value, onChange }: InputProps<Array<string>>){
   </div>;
 }
 
-import { ToolTipSpan } from "../../../../components/ToolTip";
+import { ToolTipSpan } from "../../../../../components/ToolTip";
 function PathVariableItem({ index, value: variableList, onChange: setVariableList }: (
   & { index: number }
   & InputProps<Array<string>>
