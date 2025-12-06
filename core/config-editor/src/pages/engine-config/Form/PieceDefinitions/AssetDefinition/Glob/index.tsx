83ca67109863dd2761787bddbe0d1e3f1b882555
formula1 <samtobia@gmail.com>
2025-12-05 21:47:56 -0800

    Config Editor Feature: Glob input with syntax highlighting and custom error messages

--- core/config-editor/src/pages/engine-config/Form/PieceDefinitions/AssetDefinition/Glob/index.tsx
+++ core/config-editor/src/pages/engine-config/Form/PieceDefinitions/AssetDefinition/Glob/index.tsx
@@ -1,76 +1,135 @@
-import { useState } from "react";
-import { validateGlobItem } from "@match-lock/shared";
+import { useEffect, useState } from "react";
+import { MatchLockEngineConfig, validateGlobItem, validatePathVariablesInGlob } from "@match-lock/shared";
 import { ToolTipSpan } from "../../../../../../components/ToolTip";
 import { InputError } from "../../../../../../components/InputError";
-import { useValidator } from "../../../../../../utils/react";
-import type { InputProps } from "../../../../../../utils/react";
+import type { InputProps, ListItemProps } from "../../../../../../utils/react";
 import GlobTT from "./GlobTT.md";
+import { GlobHighlightInput } from "./GlobHighlightInput";
 
-export function GlobListInput({ value, onChange }: InputProps<Array<string>>){
-
-  return <div>
+export function GlobListInput(
+  { value, onChange, pathVariables }: (
+    & InputProps<Array<string>>
+    & { pathVariables: MatchLockEngineConfig["pieceDefinitions"][string]["pathVariables"] }
+  )
+){
+  return <>
     <h3><ToolTipSpan tip={GlobTT}>Glob</ToolTipSpan></h3>
     <div>
-      <GlobCreator value={value} onChange={onChange} />
-      {value.map((glob, i) => (
-        <GlobItem key={`${glob}-${i}`} index={i} value={value} onChange={onChange} />
+      <GlobCreator
+        onSubmit={(v) => onChange([...value, v])}
+        validator={(v) => validateGlob(v, pathVariables, -1, value)}
+        pathVariables={pathVariables}
+      />
+      {value.map((glob, index) => (
+        <GlobItem
+          key={`${index}`}
+          value={value[index]}
+          onChange={v => onChange(value.map((oldGlob, i) => i !== index ? oldGlob : v))}
+          validator={(v) => validateGlob(v, pathVariables, index, value)}
+
+          index={index}
+          items={value}
+          onDelete={() => onChange(value.filter((_, i) => i !== index))}
+          pathVariables={pathVariables}
+        />
       ))}
     </div>
-  </div>
-}
-
-function GlobCreator({ value, onChange }: InputProps<Array<string>>){
-  const [newGlob, setNewGlob] = useState("");
-  const error = useValidator(newGlob, validateGlobItem);
-  return <>
-    <div>Add Glob</div>
-    <div>
-      <input
-        type="text"
-        value={newGlob}
-        onChange={(e)=>{
-          setNewGlob(e.target.value)
-        }}
-      />
-      <button onClick={() => onChange([...value, newGlob])}>Add</button>
-    </div>
-    {error !== null && <InputError>{error}</InputError>}
   </>
 }
 
-function GlobItem({ index, value: globList, onChange: setGlobList }: (
-  & { index: number }
-  & InputProps<Array<string>>
-)){
-  const value = globList[index];
-  const onChange = (newGlob: string)=>(setGlobList(
-    globList.map((oldGlob, i) => i !== index ? oldGlob : newGlob)
-  ));
-  const error = useValidator(value, (value)=>{
-    lookForDuplicate(index, value, globList);
-    validateGlobItem(value);
-  });
-  // const error = useValidator(value, validateGlobItem);
+function GlobCreator(
+  { onSubmit, validator, pathVariables }: {
+    validator: (v: string)=>unknown,
+    onSubmit: (v: string)=>unknown,
+    pathVariables: Array<string>,
+  }
+){
+  const [newGlob, setNewGlob] = useState("");
+  const [error, setError] = useState<null | string>(null);
   return <>
-    <div >
-      <input
-        type="text"
-        value={value}
-        onChange={e => onChange(e.target.value)}
+    <div>Add Glob</div>
+    <form onSubmit={(e)=>{
+        e.preventDefault();
+        try{
+          if(error !== null) return;
+          if(newGlob === "") throw new Error("Glob cannot be empty");
+          validator(newGlob);
+          onSubmit(newGlob);
+          setNewGlob("");
+        }catch(e){
+          setError((e as Error).message);
+        }
+      }}>
+      <GlobHighlightInput
+        value={newGlob}
+        onChange={(v)=>{
+          try {
+            setError(null);
+            setNewGlob(v);
+            validator(v);
+          }catch(error){
+            setError((error as Error).message);
+          }
+        }}
+        placeholder="e.g. **/*.png"
+        className="glob-highlight-input"
+        pathVariables={pathVariables}
       />
       <button
-        onClick={()=> setGlobList(globList.filter((_, i) => i !== index))}
+        disabled={newGlob === "" || error !== null}
+        type="submit"
+        >Add</button>
+    </form>
+    {error !== null && <div className="error">{error}</div>}
+  </>
+}
+
+function GlobItem({ value, onChange, onDelete, validator, pathVariables }: (
+  & InputProps<string>
+  & ListItemProps<string>
+  & { validator: (v: string)=>unknown }
+  & { pathVariables: Array<string> }
+)){
+  const [newGlob, setNewGlob] = useState(value);
+  const [error, setError] = useState<null | string>(null);
+
+  useEffect(()=>{
+    if(error) return;
+    onChange(newGlob);
+  },[newGlob])
+
+  return <>
+    <div >
+      <GlobHighlightInput
+        value={newGlob}
+        onChange={(v)=>{
+          try {
+            setNewGlob(v);
+            setError(null);
+            validator(v);
+          }catch(error){
+            setError((error as Error).message);
+          }
+        }}
+        className="glob-highlight-input"
+        pathVariables={pathVariables}
+      />
+      <button
+        onClick={()=> onDelete()}
       >Remove</button>
     </div>
     {error !== null && <InputError>{error}</InputError>}
   </>;
 }
 
-function lookForDuplicate(index: number, item: string, list: Array<string>){
+function validateGlob(
+  value: string, pathVariables: Array<string>, index: number, list: Array<string>
+){
+  validatePathVariablesInGlob(value, pathVariables);
+  validateGlobItem(value);
+  // Check for duplicate globs
   for(let i = 0; i < list.length; i++){
     if(i === index) continue;
-    if(list[i] === item){
-      throw new Error("Duplicate Glob");
-    }
+    if(list[i] === value) throw new Error("Duplicate Glob");
   }
 }
