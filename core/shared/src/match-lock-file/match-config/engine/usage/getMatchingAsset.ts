8b5fd3731625e60ae0a1791b8140f6ea90522dc5
formula1 <samtobia@gmail.com>
2025-12-08 14:04:25 -0800

    Config Fix: asset file matching now expects path variables

--- core/shared/src/match-lock-file/match-config/engine/usage/getMatchingAsset.ts
+++ core/shared/src/match-lock-file/match-config/engine/usage/getMatchingAsset.ts
@@ -3,19 +3,36 @@ import type {
   MatchLockEngineConfig,
 } from '../types';
 
+import { validatePathVariableValues } from "../../piece/validate/definition/pathVariables";
 /**
  * Test a single file against piece definition assets
  */
 export function getMatchingAssetsForFile(
   pieceDefinition: MatchLockEngineConfig['pieceDefinitions'][string],
+  pathVariables: Record<string, string>,
   relativePath: string,
 ): Array<MatchLockEngineConfig['pieceDefinitions'][string]['assets'][number]> {
+
+  validatePathVariableValues(pieceDefinition, pathVariables);
+
   return pieceDefinition.assets.filter((asset) =>(
-    asset.glob.some((glob)=>(
+    asset.glob.some((glob)=>{
+      // Replace path variables
+      glob = replacePathVariables(glob, pathVariables);
       // check if the relativePath is exactly the glob
-      relativePath === glob || 
-      // or check if the relativePath matches the glob
-      micromatch.isMatch(relativePath, glob)
-    ))
+      if(relativePath === glob) return true;
+      // check if the relativePath matches the glob
+      if(micromatch.isMatch(relativePath, glob)) return true;
+      return false;
+    })
   ));
+}
+
+function replacePathVariables(
+  glob: string, pathVariables: Record<string, string>
+){
+  for(const [variableName, value] of Object.entries(pathVariables)){
+    glob = glob.replaceAll(`<${variableName}>`, value);
+  }
+  return glob;
 }
