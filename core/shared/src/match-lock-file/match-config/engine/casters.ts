f416b3432d78edb9add6b2a5eed273a02765be87
formula1 <samtobia@gmail.com>
2025-12-08 14:03:30 -0800

    Type Fix: don't validate the caster so that we can save/load cast empty lists or slightly invalid configuration

--- core/shared/src/match-lock-file/match-config/engine/casters.ts
+++ core/shared/src/match-lock-file/match-config/engine/casters.ts
@@ -12,10 +12,8 @@ import {
   Union as CastUnion,
   String as CastString,
 } from "runtypes"
-import { MatchLockEngineAssetDefinition, MatchLockEngineConfig } from "./types";
 
-
-import { validateRange, validateGlobItem } from "./validate";
+import { MatchLockEngineConfig } from "./types";
 
 export const MatchLockEngineCaster = CastObject({
   name: CastString,
@@ -24,32 +22,14 @@ export const MatchLockEngineCaster = CastObject({
     pathVariables: CastArray(CastString),
     assets: CastArray(CastObject({
       name: CastString,
-      glob: CastArray(CastString).withConstraint((value) => {
-        try {
-          if(value.length === 0) throw new Error("Glob list is empty");
-          if(new Set(value).size !== value.length) throw new Error("Duplicate globs");
-          for(const g of value){
-            validateGlobItem(g);
-          }
-          return true;
-        }catch(e){
-          return (e as Error).message;
-        }
-      }),
+      glob: CastArray(CastString),
       classification: CastUnion(Literal("logic"), Literal("media"), Literal("doc")),
       count: CastUnion(
         CountCaster,
         Literal("*"),
         CastTuple(CountCaster, CountCaster),
         CastTuple(CountCaster, Literal("*")),
-      ).withConstraint((value) => {
-        try {
-          validateRange(value);
-          return true;
-        }catch(e){
-          return (e as Error).message;
-        }
-      })
+      )
     }))
   })),
 }).conform<MatchLockEngineConfig>();
