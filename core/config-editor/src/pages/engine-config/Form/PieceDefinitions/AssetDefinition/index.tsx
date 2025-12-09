3f784583e0824ae035f97e95f22b3ca28c422ecb
formula1 <samtobia@gmail.com>
2025-12-08 20:51:58 -0800

    Config Editor Feature: display a link to the new asset when creating one

--- core/config-editor/src/pages/engine-config/Form/PieceDefinitions/AssetDefinition/index.tsx
+++ core/config-editor/src/pages/engine-config/Form/PieceDefinitions/AssetDefinition/index.tsx
@@ -6,6 +6,7 @@ import { AssetDefinitionForm } from "./AssetItem";
 import { MatchLockEngineConfig } from "@match-lock/shared";
 
 import tooltip from "./tooltip.md";
+import { useEffect, useState } from "react";
 
 export function AssetsInput(
   { value, onChange, pathVariables }: (
@@ -13,25 +14,45 @@ export function AssetsInput(
     & { pathVariables: MatchLockEngineConfig["pieceDefinitions"][string]["pathVariables"] }
   )
 ){
+  const [newAsset, setNewAsset] = useState<null | string>(null);
+
+  useEffect(()=>{
+    const to = setTimeout(()=>(setNewAsset(null)), 10 * 1000);
+    return () => clearTimeout(to);
+  }, [newAsset])
+
   return <>
     <h3><ToolTipSpan tip={tooltip} clickable>Assets</ToolTipSpan></h3>
     <AssetDefinitionCreator
       validate={(v)=>{
         if(value.some((a) => a.name === v)) throw new Error("Name already exists")
       }}
-      onSubmit={(v) => onChange([...value, v])}
+      onSubmit={(v) =>{
+        onChange([...value, v])
+        setNewAsset(v.name);
+      }}
     />
-    {value.map((asset, i) => (
-      <div className="section" key={i}>
-        <AssetDefinitionForm
-          index={i}
-          value={asset}
-          onChange={v => onChange(value.map((a, j) => j === i ? v : a))}
-          onDelete={() => onChange(value.filter((_, j) => j !== i))}
-          items={value}
-          pathVariables={pathVariables}
-        />
-      </div>
-    ))}
+    {newAsset !== null && <div className="error">
+      <a href={`#${assetElementId(newAsset)}`}>New asset created: {newAsset}</a>
+    </div>}
+
+    <div className="alternate-list">
+      {value.map((asset, i) => (
+        <div className="section" key={i} id={assetElementId(asset.name)}>
+          <AssetDefinitionForm
+            index={i}
+            value={asset}
+            onChange={v => onChange(value.map((a, j) => j === i ? v : a))}
+            onDelete={() => onChange(value.filter((_, j) => j !== i))}
+            items={value}
+            pathVariables={pathVariables}
+          />
+        </div>
+      ))}
+    </div>
   </>;
+}
+
+function assetElementId(assetName: string){
+  return `engine-piece-asset-${assetName}`;
 }
