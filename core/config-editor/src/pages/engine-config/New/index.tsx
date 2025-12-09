435401588da729673517dcb3688f3dc047348bd7
formula1 <samtobia@gmail.com>
2025-12-09 14:54:06 -0800

    Config Editor Refactor: new engine config now uses follow button form

--- core/config-editor/src/pages/engine-config/New/index.tsx
+++ core/config-editor/src/pages/engine-config/New/index.tsx
@@ -10,6 +10,8 @@ import { useNavigate } from "react-router";
 import { replaceParams } from "../../../utils/router";
 import { EngineConfigPaths } from "../paths";
 
+import { FollowButtonForm } from "../../../components/FollowButtonForm";
+
 export function NewEngineConfig(){
   const navigate = useNavigate();
   const [value, setValue] = useState<MatchLockEngineConfig>({
@@ -18,27 +20,35 @@ export function NewEngineConfig(){
     pieceDefinitions: {},
   });
 
-  return <div>
+  return <div style={{ overflow: "hidden", flexGrow: 1 }}>
     <h1>New Engine Config</h1>
-    <div>
-      <button
-        onClick={async ()=>{
-          const filename = keyToFilename(value.name);
-          const matchlockDir = await FS.getMatchLockDir();
-          const { canceled, filePath } = await WINDOW.showSaveDialog({
-            title: 'Save Engine Config',
-            defaultPath: pathJoin(matchlockDir,`${filename}.matchlock.engine.json`),
-            filters: [
-              { name: 'JSON Files', extensions: ['json'] }
-            ]
-          })
-          if(canceled || !filePath) return;
+    <FollowButtonForm
+      info={{
+        title: "New Engine Config",
+        note: null,
+      }}
+      buttons={[
+        {
+          label: "Save",
+          onClick: async ()=>{
+            const filename = keyToFilename(value.name);
+            const matchlockDir = await FS.getMatchLockDir();
+            const { canceled, filePath } = await WINDOW.showSaveDialog({
+              title: 'Save Engine Config',
+              defaultPath: pathJoin(matchlockDir,`${filename}.matchlock.engine.json`),
+              filters: [
+                { name: 'JSON Files', extensions: ['json'] }
+              ]
+            })
+            if(canceled || !filePath) return;
 
-          await FS.writeFile(filePath, new TextEncoder().encode(JSON.stringify(value, null, 2)));
-          navigate(replaceParams(EngineConfigPaths.edit, { enginePath: encodeURIComponent(filePath) }))
-        }}
-      >Save</button>
-    </div>
-    <EngineConfigForm value={value} onChange={setValue} />
+            await FS.writeJSON(filePath, value);
+            navigate(replaceParams(EngineConfigPaths.edit, { enginePath: encodeURIComponent(filePath) }))
+          }
+        }
+      ]}
+    >
+      <EngineConfigForm value={value} onChange={setValue} />
+    </FollowButtonForm>
   </div>
 }
