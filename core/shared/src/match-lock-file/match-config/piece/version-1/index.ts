9e8603327756f11f70eb0c8bb235e4a22591917a
formula1 <samtobia@gmail.com>
2025-12-16 00:43:33 -0800

    Config Feature: exporting roster with engine

--- core/shared/src/match-lock-file/match-config/piece/version-1/index.ts
+++ core/shared/src/match-lock-file/match-config/piece/version-1/index.ts
@@ -4,10 +4,20 @@ export * from "./types";
 export * from "./validate";
 export * from "./caster-json-schema";
 
-import { engineKeywords } from "../../engine/caster-json-schema";
+import { engineSchema, engineKeywords } from "../../engine/caster-json-schema";
 import { rosterLockPiecesSchema, rosterLockPieceKeywords } from "./caster-json-schema";
+import { RosterLockEngineWithRosterConfig } from "./types";
 
-export const ROSTERLOCK_ENGINE_WITH_ROSTER_CASTER_JSONSCHEMA = new JSONSchemaCaster(
-  rosterLockPiecesSchema, [...engineKeywords, ...rosterLockPieceKeywords]
+export const ROSTERLOCK_ENGINE_WITH_ROSTER_CASTER_JSONSCHEMA = new JSONSchemaCaster<RosterLockEngineWithRosterConfig>(
+  {
+    type: "object",
+    required: ["engine", "pieces"],
+    additionalProperties: false,
+    properties: {
+      engine: engineSchema,
+      pieces: rosterLockPiecesSchema,
+    },
+  },
+  [...engineKeywords, ...rosterLockPieceKeywords]
 )
 
