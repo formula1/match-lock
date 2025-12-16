28b2ae9a064766e16322b1b3abc4295e2125aa7c
formula1 <samtobia@gmail.com>
2025-12-16 00:42:28 -0800

    Config Fix: export the engineSchema so it can be used by the other configs

--- core/shared/src/match-lock-file/match-config/engine/caster-json-schema/index.ts
+++ core/shared/src/match-lock-file/match-config/engine/caster-json-schema/index.ts
@@ -3,7 +3,7 @@
 import { engineSchema, engineKeywords } from "./schema";
 import { JSONSchemaCaster } from "../../../util-types/json-schema";
 
-export { engineKeywords };
+export { engineSchema, engineKeywords };
 export const ROSTERLOCK_ENGINE_CASTER_JSONSCHEMA = new JSONSchemaCaster(
   engineSchema, engineKeywords
 )
