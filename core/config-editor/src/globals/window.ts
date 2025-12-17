a9b3465e3d265ed472f7b62bc10284dc2e4793fd
formula1 <samtobia@gmail.com>
2025-12-16 23:22:36 -0800

    Config Editor Cleanup: just clone the native api instead of wrapping them

--- core/config-editor/src/globals/window.ts
+++ core/config-editor/src/globals/window.ts
@@ -3,14 +3,5 @@ import { nativeAPI } from "../tauri";
 
 
 export const WINDOW = {
-  minimize: () => nativeAPI.nativeWindow.minimize(),
-  maximize: () => nativeAPI.nativeWindow.maximize(),
-  close: () => nativeAPI.nativeWindow.close(),
-  toggleDevTools: () => nativeAPI.toggleDevTools(),
-  showOpenDialog: (...args: Parameters<typeof nativeAPI.nativeWindow.showOpenDialog>)=>(
-    nativeAPI.nativeWindow.showOpenDialog(...args)
-  ),
-  showSaveDialog: (...args: Parameters<typeof nativeAPI.nativeWindow.showSaveDialog>)=>(
-    nativeAPI.nativeWindow.showSaveDialog(...args)
-  ),
+  ...nativeAPI.nativeWindow,
 }
